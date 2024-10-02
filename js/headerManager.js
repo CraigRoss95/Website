ca978f180234f51bc3961771acd4d795b4dfb289
My Desktop <39316467+CraigRoss95@users.noreply.github.com>
2024-10-01 20:35:44 -0400

    Finish header manager functionality

---
+++ js/headerManager.js
@@ -0,0 +1,33 @@
+
+class SpecialHeader extends HTMLElement {
+
+    connectedCallback() { 
+        var data;
+        
+        $.ajax({
+            url: "header.html",
+            data: data,
+            success: function (data) {
+                var fileName = location.href.split("/").slice(-1); 
+                document.getElementById('special-header').innerHTML= data;
+                switch(fileName[0])
+                {
+
+                    case "portfolio.html":
+                        document.getElementById("nav-bar-portfolio").className += "nav-bar-selected";
+                        break;
+                    case "other.html":
+                        document.getElementById("nav-bar-other").className += "nav-bar-selected";   
+                        break; 
+                    default:
+                        document.getElementById("nav-bar-about-me").className += "nav-bar-selected";
+                        break;
+                }
+            },
+            dataType: "text"
+        });
+
+    }
+}
+
+customElements.define('special-header', SpecialHeader);
